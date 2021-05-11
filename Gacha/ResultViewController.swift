3ddcebf9aa1da9b12f1dadd952164acf3a3dca0c
Tamako Aiba <t.aiba@4nonome.com>
2021-05-11 23:01:09 +0900

    ガチャアプリ完成

---
+++ Gacha/ResultViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ResultViewController.swift
+//  Gacha
+//
+//  Created by tmk_cheer on 2021/05/11.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var number: Int!
+    
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var monsterImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        number = Int.random(in: 0...9)
+        if number == 9{
+            monsterImageView.image = UIImage(named:"monster010")
+            backgroundImageView.image = UIImage(named:"bg_gold")
+        }else if number > 7{
+            monsterImageView.image = UIImage(named:"monster006")
+            backgroundImageView.image = UIImage(named:"bg_red")
+        }else {
+            monsterImageView.image = UIImage(named: "monster003")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+        }
+    }
+    @IBAction func back(){
+        self.dismiss(animated:true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
